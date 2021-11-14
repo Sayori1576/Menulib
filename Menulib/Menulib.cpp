30731aefdfb0707b7721de2e6f5f04b641809f48
Sayori <arwen02@naver.com>
2021-11-14 22:32:17 +0900

    최초 버전

--- Menulib/Menulib.cpp
+++ Menulib/Menulib.cpp
@@ -1,11 +1,19 @@
 ﻿// Menulib.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
 
-#include <iostream>
-
+#include "Menulib.h"
+void printh()
+{
+    cout << "ㅎㅇㅇ" << endl;
+}
+void printn()
+{
+    cout << "1234" << endl;
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    Menu M({ {"숫자 출력",printn},{"문자 출력",printh} });
+    M.run();
 }
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
